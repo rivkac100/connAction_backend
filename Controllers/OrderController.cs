8b065a83dc6471e6b4bfcd217f127209a617b7a8
chavi-rotenberg <cr327744694@sgur.org.il>
2025-03-31 13:15:14 +0300

    CREATE c# SERVER

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,89 @@
+﻿//בס"ד
+
+using BL.Api;
+using BL.Models;
+using Dal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        IBlOrder orders;
+        public OrderController(IBl manager) 
+        {
+            orders = manager.Order;
+        }
+        [HttpGet("GetAll")]
+        public IActionResult Get()
+        {
+            if (orders.Get() == null)
+            {
+                return NotFound("Not Found");
+            }
+            return Ok(orders.Get());
+        }
+        [HttpGet("GetByid/{id}")]
+        public IActionResult Get(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound("Not Found Id");
+            }
+            if (id < 0)
+            {
+                return BadRequest("Invalid Id");
+            }
+            return Ok(orders.GetById(id));
+        }
+        [HttpGet("GetByCustomerId/{customerId}")]
+        public IActionResult GetByCustomerId(int customerId)
+        {
+            if (customerId == 0)
+            {
+                return BadRequest("Invalid Id ");
+            }
+            return Ok(orders.GetByCustomerId(customerId));
+        }
+        [HttpDelete("Delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                orders.Delete(id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return Ok(orders.Get());
+        }
+        [HttpPut("Update")]
+        public IActionResult Put(BlOrder c)
+        {
+            try
+            {
+                orders.Update(c);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return Ok(orders.Get());
+        }
+        [HttpPost("Add")]
+        public void Add(BlOrder c)
+        {
+            try
+            {
+                orders.Create(c);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
